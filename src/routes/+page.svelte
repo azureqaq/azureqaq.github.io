e68e312ebaeca0a3d36eb82c7c2f426e07b707f6
azureqaq <112307109+azureqaq@users.noreply.github.com>
2025-12-17 10:26:48 +0000

    update keys

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,14 @@
 <ul>
     <li>
         OpenPGP key:
+        <code>E571 5F4B C9A8 1353 C688  21BD 0316 B34D A0CF E7AD</code>
+    </li>
+    <li>
+        OpenPGP key:
         <code>BFD4 B724 4D9A 51FC FEF4  98CB 7D26 D9B2 4722 CBA0</code>
     </li>
     <li>
-        Download: <a href="https://github.com/azureqaq.gpg">azure.gpg</a>
+        Download: <a href="/azureqaq.gpg">azureqaq.gpg</a>
     </li>
 </ul>
 
@@ -16,3 +20,4 @@
     <img src="/favicon-32x32.png" alt="my gravatar" />
     <img src="/favicon.ico" alt="my gravatar" />
 </div>
+
